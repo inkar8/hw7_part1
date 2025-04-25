8aed2a155e17c713d488eaa5cf5ba7e3c5edc3c2
Естеу Инкар <esteuinkar@MacBook-Air-Esteu.local>
2025-04-25 22:50:14 +0500

    Episode class with title and runtime properties

---
+++ src/model/Episode.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Episode {
+    private String title;
+    private int runtimeSec;
+
+    public Episode(String title, int runtimeSec) {
+        this.title = title;
+        this.runtimeSec = runtimeSec;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getRuntimeSec() {
+        return runtimeSec;
+    }
+}
